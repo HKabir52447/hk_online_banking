3f5475772a77bc82a1632d75662ce335cf02346e
MD. Humayun Kabir <humayunkabir52447@gmail.com>
2021-08-15 17:44:12 +0600

    input validation correction

--- javascript/banking.js
+++ javascript/banking.js
@@ -6,16 +6,22 @@ document.getElementById('deposit-btn').addEventListener('click', function(e){
     let withdrawInput = document.getElementById('withdraw');
     let depoAmount = document.getElementById('depoAmount');
     let currentAmount = depoAmount.innerText;
-    let currentTk = parseFloat(currentAmount);
-    let inputTk = parseFloat(depoInputValue);
-    let totalTk = currentTk + inputTk;
-    depoAmount.innerText = totalTk;
-    depositInput.value = '';
-    let getBlance = document.getElementById('blance');
-    let blanceAmount = getBlance.innerText;
-    let currentBlance = parseFloat(blanceAmount);
-    let updateBlance = currentBlance + inputTk;
-    getBlance.innerText = updateBlance;
+    if(depoInputValue <= 0){
+        alert('Please enter valid amount greater than 0');
+    }
+    else{
+        let currentTk = parseFloat(currentAmount);
+        let inputTk = parseFloat(depoInputValue);
+        let totalTk = currentTk + inputTk;
+        depoAmount.innerText = totalTk;
+        depositInput.value = '';
+        let getBlance = document.getElementById('blance');
+        let blanceAmount = getBlance.innerText;
+        let currentBlance = parseFloat(blanceAmount);
+        let updateBlance = currentBlance + inputTk;
+        getBlance.innerText = updateBlance;
+    }
+   
 });
 
 document.getElementById('withdraw-btn').addEventListener('click', function(e){
@@ -24,19 +30,25 @@ document.getElementById('withdraw-btn').addEventListener('click', function(e){
     let withdrawText = getWithdraw.innerText;
     let getWithdraWInput = document.getElementById('withdrawinput');
     let withdrawInputValue = getWithdraWInput.value;
-    getWithdraWInput.value = '';
-    let getBlance = document.getElementById('blance');
-    let blanceAmount = getBlance.innerText;
-    let currentBlance = parseFloat(blanceAmount);
-    let withdrawBlance = parseFloat(withdrawInputValue)
-    if(withdrawBlance <= currentBlance){
-        let totalWithdraw = parseFloat(withdrawText) + parseFloat(withdrawInputValue);
-        getWithdraw.innerText = totalWithdraw;
-        let updateBlance = currentBlance - withdrawBlance;
-        getBlance.innerText = updateBlance;
+    if(withdrawInputValue <= 0){
+        alert('Please enter valid amount greater than 0');
     }
     else{
-        alert('You have not sufficent blance')
+        getWithdraWInput.value = '';
+        let getBlance = document.getElementById('blance');
+        let blanceAmount = getBlance.innerText;
+        let currentBlance = parseFloat(blanceAmount);
+        let withdrawBlance = parseFloat(withdrawInputValue);
+        if(withdrawBlance <= currentBlance){
+            let totalWithdraw = parseFloat(withdrawText) + parseFloat(withdrawInputValue);
+            getWithdraw.innerText = totalWithdraw;
+            let updateBlance = currentBlance - withdrawBlance;
+            getBlance.innerText = updateBlance;
+        }
+        else{
+            alert('You have not sufficent blance')
+        }
     }
+  
     
 })
